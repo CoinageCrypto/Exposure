784c4a116d0df40165d3011ab756a77a6662c573
Kevin Brown <kevin.brown@coina.ge>
2018-06-28 10:27:53 +1000

    By name is better I reckon.

--- contracts/crowdsale.sol
+++ contracts/crowdsale.sol
@@ -1,14 +1,14 @@
 pragma solidity 0.4.24;
 
 import "./open-zeppelin/contracts/crowdsale/Crowdsale.sol";
-
+import "./token.sol";
 
 /**
 * @dev This contract is here to show that we're using OpenZeppelin's Crowsdsale contract.
 */
 
 contract ExposureCrowdSale is Crowdsale {
-    constructor(uint256 rate_, address wallet_, ERC20 token_) public 
+    constructor(uint256 rate_, address wallet_, ExposureToken token_) public 
         Crowdsale(rate_, wallet_, token_)
     {
         // We don't need to do anything here. It's all already been written.
