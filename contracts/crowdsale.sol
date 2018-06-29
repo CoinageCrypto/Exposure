212cc8e1354487c82c4db9263e719fe6afb675ae
Kevin Brown <kevin.brown@coina.ge>
2018-06-29 11:40:23 +1000

    Added options for owner to Crowdsale contract.

--- contracts/crowdsale.sol
+++ contracts/crowdsale.sol
@@ -1,16 +1,50 @@
 pragma solidity 0.4.24;
 
 import "./open-zeppelin/contracts/crowdsale/Crowdsale.sol";
+import "./open-zeppelin/contracts/ownership/Ownable.sol";
 import "./token.sol";
 
-/**
-* @dev This contract is here to show that we're using OpenZeppelin's Crowsdsale contract.
-*/
-
-contract ExposureCrowdSale is Crowdsale {
+contract ExposureCrowdSale is Crowdsale, Ownable {
     constructor(uint256 rate_, address wallet_, ExposureToken token_) public 
         Crowdsale(rate_, wallet_, token_)
     {
-        // We don't need to do anything here. It's all already been written.
+    }
+
+    /**
+    * @dev Allows the owner to withdraw any Exposure in the crowdsale contract
+    * @param _amount Exposure to withdraw
+    */
+    function withdraw(uint256 _amount)
+        onlyOwner
+        external
+    {
+        token.transfer(owner, _amount);
+    }
+
+    /**
+    * @dev Allows the owner to change the exchange rate
+    * @param _rate The new rate of the crowdsale
+    */
+    function updateRate(uint256 _rate)
+        onlyOwner
+        external
+    {
+        rate = _rate;
+    }
+
+    /**
+    * @dev Allows a participant to ensure they receive a specfic rate when purchasing to prevent front-running by the owner
+    * @param _beneficiary The recipient of all that sweet sweet Exposure.
+    * @param _guaranteedRate The rate the recipient wants to ensure they receive.
+    */
+    function buyTokensAtRate(address _beneficiary, uint256 _guaranteedRate)
+        external
+        payable
+    {
+        // Ensure the rate we're providing is the one the user asks for or revert the transaction.
+        require(rate == _guaranteedRate);
+
+        // Ok, we're good to execute the buy.
+        return buyTokens(_beneficiary);
     }
 }
